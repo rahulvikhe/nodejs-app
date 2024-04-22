3a1d1f7a113fae09d5474789b035013d9701c09b
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-22 22:38:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ app.use((_req, res, next) => {
     next();
   });
 
-// Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
- 
-// Parses the text as json
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
