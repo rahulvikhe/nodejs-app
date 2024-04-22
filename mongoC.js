663366121936038c2cda9d44edaa55588683825a
rahulvikhe1 <rahulvikhe1@gmail.com>
2024-04-22 17:59:46 +0530

    test deployment aws ec2

--- mongoC.js
+++ mongoC.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const connectionString = `mongodb+srv://integrationninjas:${password}@ev-cluster.zk5mtjd.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`; // clustore url
+const connectionString = `mongodb+srv://rahulvikhe:${password}@dev-cluster.idtyynl.mongodb.net/?retryWrites=true&w=majority&appName=dev-cluster`; // clustore url
 const client = new MongoClient(connectionString);
 let conn;
 try {
